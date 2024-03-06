33a0b40cfab82a6266f5d1660a2ab53538193774
Robson Souza <robsonbacchindesouza@gmail.com>
2024-03-06 08:29:49 -0300

    create cliente

--- api/src/controllers/cliente.js
+++ api/src/controllers/cliente.js
@@ -2,13 +2,17 @@ const con = require('../connection/mysql');
 
 //CRUD - CREATE
 const addCliente = (req, res) => {
-    const { nome, endereco, telefone, email } = req.body;
-    con.query('INSERT INTO cliente (nome, endereco, telefone, email) VALUES (?, ?, ?, ?)', [nome, endereco, telefone, email], (err, result) => {
-        if (err) {
-            res.status(500).send('Erro ao adicionar cliente');
-        }
-        res.status(201).send('Cliente adicionado com sucesso');
-    });
+    try {
+        const { nome, endereco, telefone, email } = req.body;
+        con.query('INSERT INTO cliente (nome, endereco, telefone, email) VALUES (?, ?, ?, ?)', [nome, endereco, telefone, email], (err, result) => {
+            if (err) {
+                res.status(500).send('Erro ao adicionar cliente');
+            }
+            res.status(201).send('Cliente adicionado com sucesso');
+        });
+    } catch (error) {
+        res.status(500).send('Erro ao adicionar cliente');
+    }
 }
 
 //CRUD - READ
@@ -22,5 +26,6 @@ const getClientes = (req, res) => {
 }
 
 module.exports = {
+    addCliente,
     getClientes
 }
--- api/src/routes.js
+++ api/src/routes.js
@@ -7,6 +7,7 @@ routes.get('/', (req, res) => {
     res.json("API Transportadora XPTO 1.0")
 });
 
+routes.post('/clientes', Cliente.addClientes);
 routes.get('/clientes', Cliente.getClientes);
 
 module.exports = routes;
